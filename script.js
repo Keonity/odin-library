1509fecd5691976564bed782c25ed75315af1eab
Keonity <56704285+Keonity@users.noreply.github.com>
2024-06-21 01:11:34 -0700

    Added new book dialog and cleaned up current styling.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const myLib = [];
 
-function Book(title, author, pages) {
+function Book(title, author, isRead) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
+    this.isRead = isRead;
 }
 
 function addBookToLibrary(book) {
@@ -11,19 +11,36 @@ function addBookToLibrary(book) {
         myLib.push(book);
     }
 }
-
-const enchanted = new Book("Enchanted", "Ariva Derice", 32);
+const enchanted = new Book("Enchanted", "Ariva Derice", "Read");
 addBookToLibrary(enchanted);
 
-const yuna = new Book("Yuna, Dark City", "Anonymous", 362);
+const yuna = new Book("Yuna, Dark City", "Anonymous", "Unread");
 addBookToLibrary(yuna);
 
 const library = document.querySelector(".library");
 
+const newBookBtn = document.querySelector("#newBook");
+
+const addBookDialog = document.querySelector("#addBook");
+
+const closeBookDialog = document.querySelector("#closeBook");
+
+window.onload = function() {
+    addBookDialog.close();
+};
+
+newBookBtn.addEventListener("click", () => {
+    addBookDialog.showModal();
+});
+
+closeBookDialog.addEventListener("click", () => {
+    addBookDialog.close();
+});
+
 myLib.forEach((element, index, array) => {
     var newBook = document.createElement("div");
     newBook.setAttribute("class", "book");
-    var title = document.createElement("p");
+    var title = document.createElement("h3");
     title.setAttribute("id", element.title);
     var titleNode = document.createTextNode(`${element.title}`);
     title.appendChild(titleNode);
@@ -31,12 +48,12 @@ myLib.forEach((element, index, array) => {
     author.setAttribute("id", element.author);
     var authorNode = document.createTextNode(`${element.author}`);
     author.appendChild(authorNode);
-    var pages = document.createElement("p");
-    pages.setAttribute("id", element.pages);
-    var pageNode = document.createTextNode(`${element.pages}`);
-    pages.appendChild(pageNode);
+    var isRead = document.createElement("p");
+    isRead.setAttribute("id", element.isRead);
+    var isReadNode = document.createTextNode(`${element.isRead}`);
+    isRead.appendChild(isReadNode);
     newBook.appendChild(title);
     newBook.appendChild(author);
-    newBook.appendChild(pages);
+    newBook.appendChild(isRead);
     library.appendChild(newBook);
 });
